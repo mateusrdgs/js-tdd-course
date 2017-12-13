0a13cc375e2e40f761a1aa8c9306d45046c8cd00
Mateus Rodrigues <devrdgs@gmail.com>
2017-12-13 16:46:03 -0200

    Added nyc

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-/* eslint-env node */
-
-const sum = (num1, num2) => num1 + num2;
-const sub = (num1, num2) => num1 - num2;
-const mult = (num1, num2) => num1 * num2;
-const div = (num1, num2) => (num2 === 0 ? 'Division by zero isn\'t possible!' : num1 / num2);
-
-export {
-  sum,
-  sub,
-  mult,
-  div,
-};
 
